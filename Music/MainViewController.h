07a02a229953ce010fdbac21c77047601301a89f
RaySaber <790903619@qq.com>
2015-11-25 17:20:01 +0800

    first commit

---
+++ Music/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Music
+//
+//  Created by Ray on 15/6/25.
+//  Copyright (c) 2015年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
